75c65e5377aec3852f68a46478e4f74ab5082bd6
munetracker <mune.tracker@gmail.com>
2024-06-06 07:05:49 +0800

    June 6 2024 E

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/will-you-be-my-valentine",
+  base: '/bemyvalentine',
   plugins: [react()],
+  build: {
+    sourcemap: true, // Enable source maps for production build
+  }
 });
